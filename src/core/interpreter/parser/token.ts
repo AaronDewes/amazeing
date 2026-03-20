9f8eb3d8b884e698f6ebc319f0697019d95986bb
Clickism <yagizaktasx@gmail.com>
2026-03-20 11:31:05 +0100

    add tokens

---
+++ src/core/interpreter/parser/token.ts
@@ -0,0 +1,15 @@
+type Tok<T extends string, V = undefined> = {
+  type: T;
+} & (V extends undefined ? object : { value: V });
+
+/**
+ * Token type
+ */
+export type Token =
+  | Tok<"identifier", string>
+  | Tok<"number", number>
+  | Tok<"char", number>
+  | Tok<"colon">
+  | Tok<"lbracket">
+  | Tok<"rbracket">
+  | Tok<"newline">;
